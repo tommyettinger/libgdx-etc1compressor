4bc25edbe370fb8989ea30761b6b5f1faa1ed1cf
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-11-06 20:39:31 -0800

    Graal seems to work here.

--- graalvm-env/build_native.bat
+++ graalvm-env/build_native.bat
@@ -3,11 +3,10 @@ call C:\d\jvm\graal17\bin\native-image.cmd ^
 -H:+ReportExceptionStackTraces ^
 --report-unsupported-elements-at-runtime ^
 --no-fallback ^
---allow-incomplete-classpath ^
 -H:ReflectionConfigurationFiles=config/reflect-config.json ^
 -H:JNIConfigurationFiles=config/jni-config.json ^
 -H:DynamicProxyConfigurationFiles=config/proxy-config.json ^
 -H:SerializationConfigurationFiles=config/serialization-config.json ^
 -H:ResourceConfigurationFiles=config/resource-config.json ^
 -Dorg.lwjgl.librarypath=. ^
--jar noisegen-0.1.2.jar
+-jar libgdx-etc1compressor-runnable-1.11.0.0.jar
